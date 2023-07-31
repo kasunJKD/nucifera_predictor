0628beed3449ffb6745ffd6423554f0cc063310f
kasunjayakody <kasun@project99.gg>
2023-07-31 19:25:49 +0530

    frontend added

---
+++ frontend/src/environments/environment.instance.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  development: false,
+  instance: true,
+  baseurl: 'http://kasun.dev.sandbox.project99.gg:48080/',
+  otpbaseurl: 'http://kasun.dev.sandbox.project99.gg:48090/',
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  development: true,
+  instance: false,
+  baseurl: 'http://localhost:48080/',
+};
