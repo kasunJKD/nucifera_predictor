36842c3f3bdf69368c0b18e8dd0a5e28884dd61a
kasunJKD <kasun.ravihara@gmail.com>
2023-09-22 05:36:39 +0530

    fixed 1d issue

--- backendFlask/model.py
+++ backendFlask/model.py
@@ -219,12 +219,15 @@ def predictGRU ():
     return 'GRU fired'
 
 def predict1D ():
+    print("hit1")
     conn = psycopg2.connect(database="nuciferaDB", user="postgres", password="9221", host="nucifera-db", port="5432")
     cursor = conn.cursor()
     query = "SELECT * FROM batch1.original ORDER BY average_price DESC;"
     df = pd.read_sql_query(query, conn)
     td = pd.to_datetime(df['date'], dayfirst=True, unit='s')
     train_dates = td.sort_values(ascending=False)
+
+    print("hit2")
 
     cols = list(df)[1:]
     df_for_training = df[cols].astype(float)
@@ -238,13 +241,15 @@ def predict1D ():
     trainY = []
 
     n_future = 1   # Number of weeks we want to look into the future based on the past weeks.
-    n_past = 8  # Number of past weeks we want to use to predict the future.
+    n_past = 4  # Number of past weeks we want to use to predict the future.
 
     for i in range(n_past, len(df_for_training_scaled) - n_future +1):
         trainX.append(df_for_training_scaled[i - n_past:i, 0:df_for_training.shape[1]])
         trainY.append(df_for_training_scaled[i + n_future - 1:i + n_future, 0])
 
     trainX, trainY = np.array(trainX), np.array(trainY)
+
+    print("hit3")
 
     # define the Autoencoder model
     model = Sequential()
@@ -272,6 +277,8 @@ def predict1D ():
     plt.plot(history.history['val_loss'], label='Validation loss')
     plt.legend()
 
+    print("hit4")
+
     #A lower MSE indicates better model performance.
     MSE = model.evaluate(trainX, trainY)
 
@@ -280,37 +287,44 @@ def predict1D ():
     buffer.seek(0)
     image_base64_validation = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
+    print("hit44")
+
     n_past = 200
     n_weeks_for_prediction=100
     #prediction period should be from 2023 5 1 - 7- 14 - 21 - 27
-    predict_period_dates = pd.date_range(list(train_dates)[1], periods=n_weeks_for_prediction, freq='W-SUN').tolist()
+    predict_period_dates = pd.date_range(list(train_dates)[n_past], periods=n_weeks_for_prediction, freq='W-SUN').tolist()
 
     prediction = model.predict(trainX[:n_weeks_for_prediction])
 
     prediction_copies = np.repeat(prediction, df_for_training.shape[1], axis=-1)
     y_pred_future = scaler.inverse_transform(prediction_copies)[:,0]
 
+    print("hit4444")
     forecast_dates = []
     for time_i in predict_period_dates:
         forecast_dates.append(time_i.date())
-
+    print("hit44442323")
     df_forecast = pd.DataFrame({'date':np.array(forecast_dates), 'average_price':y_pred_future})
     df_forecast['date']=pd.to_datetime(df_forecast['date'], dayfirst=True)
+    print("hit444")
 
     original = df[['date', 'average_price']]
-    original.head
-    original['date']=pd.to_datetime(original['date'], dayfirst=True)
-
+    #original['date']=pd.to_datetime(original['date'], dayfirst=True)
+    original.loc[:, 'date'] = pd.to_datetime(original['date'], dayfirst=True)
     sns.lineplot(x=original['date'], y=original['average_price'])
     sns.lineplot(x=df_forecast['date'], y=df_forecast['average_price'])
-
+    
     buffer2 = io.BytesIO()
     plt.savefig(buffer2, format='png', bbox_inches='tight')
     buffer2.seek(0)
     image_base64_fit = base64.b64encode(buffer2.getvalue()).decode('utf-8')
 
+    print("hit5")
+
 
     mape, actualgraph = inference(df, scaled, df_for_training, model, scaler,train_dates)
+
+    print("hit6")
 
     cursor.execute('''
         INSERT INTO batch1.models (Model_Id, Model_Name, Plot_Fit, Plot_Validation, Actual_Precited_Graph,no_features, feature_list, mse, mape)
