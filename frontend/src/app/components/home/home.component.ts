8deba303d0af5788a38c2b685b4823a9d78df3d4
kasunJKD <kasun.ravihara@gmail.com>
2023-10-14 02:21:27 +0530

    dashboard done

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -9,8 +9,20 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 
 export class HomeComponent implements OnInit {
-  imageSrc: SafeResourceUrl; // Variable to store the image data
   data: any; // Variable to store the response data
+
+  lstmplotFit: SafeResourceUrl;
+  lstmPlotValidation: SafeResourceUrl;
+  lstmTestPredictions: SafeResourceUrl;
+
+  gruplotFit: SafeResourceUrl;
+  gruPlotValidation: SafeResourceUrl;
+  gruTestPredictions: SafeResourceUrl;
+
+
+  dplotFit: SafeResourceUrl;
+  dPlotValidation: SafeResourceUrl;
+  dTestPredictions: SafeResourceUrl;
 
   constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
@@ -21,13 +33,30 @@ export class HomeComponent implements OnInit {
 
     this.http.post('http://localhost:11001/api/batch/data/get', requestData).subscribe((response) => {
       this.data = response;
-      const base64Image = this.data.batchResponse[0].plotFit;
-      const decodedImageData = atob(base64Image);
-      const imageUrl = 'data:image/png;base64,' + decodedImageData;
+      // const base64Image = this.data.batchResponse[0].plotFit;
+      // const decodedImageData = atob(base64Image);
+      // const imageUrl = 'data:image/png;base64,' + decodedImageData;
 
-      console.log(imageUrl);
+      // this.imageSrc = this.sanitizer.bypassSecurityTrustResourceUrl(imageUrl);
 
-      this.imageSrc = this.sanitizer.bypassSecurityTrustResourceUrl(imageUrl);
+      if (this.data.batchResponse[0])
+      {
+        this.lstmplotFit = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + atob(this.data.batchResponse[0].plotFit));
+        this.lstmPlotValidation = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + atob(this.data.batchResponse[0].plotValidation));
+        this.lstmTestPredictions = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + atob(this.data.batchResponse[0].testPredictions));
+      }
+      if (this.data.batchResponse[1])
+      {
+        this.gruplotFit = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + atob(this.data.batchResponse[1].plotFit));
+        this.gruPlotValidation = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + atob(this.data.batchResponse[1].plotValidation));
+        this.gruTestPredictions = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + atob(this.data.batchResponse[1].testPredictions));
+      }
+      if (this.data.batchResponse[2])
+      {
+        this.dplotFit = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + atob(this.data.batchResponse[2].plotFit));
+        this.dPlotValidation = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + atob(this.data.batchResponse[2].plotValidation));
+        this.dTestPredictions = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + atob(this.data.batchResponse[2].testPredictions));
+      }
     });
   }
 }
